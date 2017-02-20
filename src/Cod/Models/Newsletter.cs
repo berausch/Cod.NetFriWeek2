7222b00016670f8a4bcf134b9c596edacdcae579
Brittany Rausch <berausch247@gmail.com>
2017-02-19 18:36:16 -0800

    Create the Basic framework for User functionality

---
+++ src/Cod/Models/Newsletter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cod.Models
+{
+    [Table("Newsletters")]
+    public class Newsletter
+    {
+        [Key]
+        public int NewsletterId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
