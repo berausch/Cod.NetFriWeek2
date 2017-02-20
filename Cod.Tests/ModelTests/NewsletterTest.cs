3b994968e072fa55e011967fa97631948c41c57f
Brittany Rausch <berausch247@gmail.com>
2017-02-19 23:07:10 -0800

    Create Testing

---
+++ Cod.Tests/ModelTests/NewsletterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cod.Models;
+using Xunit;
+
+namespace Cod.Tests
+{
+    public class NewsletterTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var newsletter = new Newsletter();
+            newsletter.Name = "Gary";
+
+            //Act
+            var result = newsletter.Name;
+
+            //Assert
+            Assert.Equal("Gary", result);
+        }
+    }
+}
